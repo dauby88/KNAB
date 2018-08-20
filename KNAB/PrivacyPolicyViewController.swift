6f80e85c41c4af78d777452f402a0028e3bf726f
Stuart Daubenmire <dauby88@icloud.com>
2018-08-20 17:05:42 -0400

    Added privacy policy

---
+++ KNAB/PrivacyPolicyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyPolicyViewController.swift
+//  KNAB
+//
+//  Created by Stuart Daubenmire on 8/20/18.
+//  Copyright © 2018 Dauby Cafe. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyPolicyViewController: UIViewController {
+
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
